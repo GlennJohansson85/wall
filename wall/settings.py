e63ea1060a1ff9eb617615402aac4bc276423205
GlennJo <g.j@live.se>
2024-12-25 15:01:54 +0100

    AWS bucket debugging

--- wall/settings.py
+++ wall/settings.py
@@ -113,13 +113,9 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
