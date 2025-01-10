826e9ddbaa105b71aa4f7c28da1f4065d549f743
GlennJo <g.j@live.se>
2025-01-10 22:01:09 +0100

    Update file-size-validation.js
    Filesize limit changed to 13mb

--- static/js/file-size-validation.js
+++ static/js/file-size-validation.js
@@ -1,10 +1,10 @@
 function validateFileSize() {
     const fileInput = document.getElementById('id_img');
     const file = fileInput.files[0];
-    const maxSize = 20 * 1024 * 1024; // 20 MB
+    const maxSize = 13 * 1024 * 1024; // 13 MB
 
     if (file.size > maxSize) {
-        alert("File size exceeds the 20 MB limit!");
+        alert("File size exceeds the 13 MB limit!");
         fileInput.value = ''; // Clear the file input
     }
 }
